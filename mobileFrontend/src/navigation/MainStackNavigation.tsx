bfa43fad5d472682158e714509abd8d5894a019f
mayanksingh121 <mt029322@gmail.com>
2025-09-15 22:43:47 +0530

    added splashscreen in android

--- mobileFrontend/src/navigation/MainStackNavigation.tsx
+++ mobileFrontend/src/navigation/MainStackNavigation.tsx
@@ -1,28 +1,44 @@
 import {StyleSheet} from 'react-native';
-import React, {useLayoutEffect, useState} from 'react';
+import React, {useLayoutEffect, useRef, useState} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoginNavigationStack from './LoginNavigationStack';
 import BottomTabNavigation from './BottomTabNavigation';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../redux/store';
-import { setUserLoginStatus } from '../redux/slices/authSlice';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, RootState} from '../redux/store';
+import {setUserLoginStatus} from '../redux/slices/authSlice';
+import SplashScreen from 'react-native-splash-screen';
 
 const MainNavigationStack = createNativeStackNavigator();
 const MainStackNavigation = () => {
-  const {isUserLoggedIn} = useSelector((store: RootState)=>store.authentication);
+  const {isUserLoggedIn} = useSelector(
+    (store: RootState) => store.authentication,
+  );
   const dispatch = useDispatch<AppDispatch>();
+  const splashScreenTimer = useRef<null | NodeJS.Timeout>(null);
 
   useLayoutEffect(() => {
     getToken();
+
+    if (splashScreenTimer?.current) {
+      clearTimeout(splashScreenTimer?.current);
+    }
+
+    splashScreenTimer.current = setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+
+    return () => {
+      if (splashScreenTimer?.current) clearTimeout(splashScreenTimer?.current);
+    };
   }, []);
 
   const getToken = async () => {
     try {
       const token = await AsyncStorage.getItem('userSessionToken');
       if (token) {
-        dispatch(setUserLoginStatus(true))
-      }else{
+        dispatch(setUserLoginStatus(true));
+      } else {
         dispatch(setUserLoginStatus(false));
       }
     } catch (e) {
@@ -31,15 +47,18 @@ const MainStackNavigation = () => {
   };
 
   return (
-    <MainNavigationStack.Navigator screenOptions={{headerShown:false}}>
-        {!isUserLoggedIn ? <MainNavigationStack.Screen
+    <MainNavigationStack.Navigator screenOptions={{headerShown: false}}>
+      {!isUserLoggedIn ? (
+        <MainNavigationStack.Screen
           name="LoginStack"
           component={LoginNavigationStack}
-        />: 
-       <MainNavigationStack.Screen
-         name="HomeStack"
-         component={BottomTabNavigation}
-        />}
+        />
+      ) : (
+        <MainNavigationStack.Screen
+          name="HomeStack"
+          component={BottomTabNavigation}
+        />
+      )}
     </MainNavigationStack.Navigator>
   );
 };
